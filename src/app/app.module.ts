60ff991e64896c2ae536e1427598b559157080f2
Jinto Jose <jintoppy@gmail.com>
2021-09-21 11:22:35 +0530

    adding new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MybuttonComponent } from './mybutton/mybutton.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MybuttonComponent
   ],
   imports: [
     BrowserModule
