3d1c1b2afe506e1b035904cbba8803d3bab1ff3e
Jinto Jose <jintoppy@gmail.com>
2021-09-21 11:44:59 +0530

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MybuttonComponent } from './mybutton/mybutton.component';
+import { SlideshowComponent } from './components/slideshow/slideshow.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MybuttonComponent
+    MybuttonComponent,
+    SlideshowComponent
   ],
   imports: [
     BrowserModule
