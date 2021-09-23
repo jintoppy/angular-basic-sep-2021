a6f36415eb4ed07f1daa4810faa5532fd3ee585d
Jinto Jose <jintoppy@gmail.com>
2021-09-23 10:06:38 +0530

    dog

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -6,15 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slideshow.component.css']
 })
 export class SlideshowComponent implements OnInit {
-  imgUrls=[
-    'https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg',
-    'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png',
-    'https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-superJumbo.jpg?quality=90&auto=webp',
-    'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg?resize=750px:*'
-  ]
-  constructor() { }
+  
+  imgUrls=[]
+  activeIndex = 0;
+  constructor() { 
+  }
 
   ngOnInit(): void {
+  }
+
+  onNext(){
+    if(this.activeIndex < this.imgUrls.length - 1){
+      this.activeIndex++;
+    }
+    
+  }
+
+  onPrev(){
+    if(this.activeIndex > 0){
+      this.activeIndex--;
+    }
+    
   }
 
 }
