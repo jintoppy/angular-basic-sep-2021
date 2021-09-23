5ddd6dfb0ad96a05e7550d5d1ff364c7ca3dd818
Jinto Jose <jintoppy@gmail.com>
2021-09-23 10:45:27 +0530

    output

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -7,18 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContainerComponent implements OnInit {
 
-  catImgUrls=[
+  catImgUrls:string[]=[
     'https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg',
     'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png',
     'https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-superJumbo.jpg?quality=90&auto=webp',
     'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg?resize=750px:*'
   ]
 
-  dogImgUrls=[
+  dogImgUrls:string[]=[
     'https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=620&quality=45&auto=format&fit=max&dpr=2&s=064680b85e72644d9cc2e69e2763c541',
     'https://media.nature.com/lw800/magazine-assets/d41586-020-01430-5/d41586-020-01430-5_17977552.jpg',
     'https://www.thesprucepets.com/thmb/kV_cfc9P4QWe-klxZ8y--awxvY4=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/adorable-white-pomeranian-puppy-spitz-921029690-5c8be25d46e0fb000172effe.jpg'
   ]
+
+  catSlideshowCompleted = false;
+  dogSlideshowCompleted = false;
+
+  onCatComplete(){
+    this.catSlideshowCompleted = true;
+  }
+
+  onDogComplete(){
+    this.dogSlideshowCompleted = true;
+  }
 
   constructor() { }
 
--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-slideshow',
@@ -6,9 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slideshow.component.css']
 })
 export class SlideshowComponent implements OnInit {
-  
-  imgUrls=[]
+  @Input()
+  imgUrls: string[]=[]
   activeIndex = 0;
+
+  @Output()
+  onComplete: EventEmitter<void> = new EventEmitter();
+
   constructor() { 
   }
 
@@ -19,7 +23,10 @@ export class SlideshowComponent implements OnInit {
     if(this.activeIndex < this.imgUrls.length - 1){
       this.activeIndex++;
     }
-    
+    if(this.activeIndex === this.imgUrls.length - 1){
+      // slideshow is completed;
+      this.onComplete.emit();
+    }
   }
 
   onPrev(){
